68290db77847b33ec5795d6da7e0c73dc722992f
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-03-31 15:20:55 -0400

    getParties & addParty functions complete

--- partyPlanner.js
+++ partyPlanner.js
@@ -1,7 +1,7 @@
-const COHORT = "2503-FTB-ET-WEB-FT";
-const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
 // https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/events
-// names, dates, times, locations, and descriptions
+// "2503-FTB-ET-WEB-FT" "2109-CPU-RM-WEB-PT"
+const COHORT = "2503-FTB-ET-WEB-FT";
+const API = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events`;
 const state = {
     party: {},
 }
@@ -10,17 +10,52 @@ async function deleteParty(name){
     render();
 }
 async function getParties(){
-
+    try{
+        const response = await fetch(API);
+        const json = await response.json();
+        return json;
+    }
+    catch(error){
+        console.error(error.message);
+    }
 }
-async function addParty(){
-
+async function addParty(Party){
+    try{
+        console.log(Party);
+        const response = await fetch(`${API}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(Party),
+        });
+        if (!response.ok) {
+            const errorData = await response.json();
+            throw new Error(`Error: ${response.status} - ${errorData.message || 'Something went wrong'}`);
+        }
+        const json = await response.json();
+    }
+    catch(error){
+        console.error(error.message);
+    }
     render();
 }
-function renderParties(){
+function renderParties(Party){
+    console.log(Party);
+    // document.addEventListener("DOMContentLoaded", function () {
+        
+    // });
 
 }
 async function render(){
-    await getParties();
-    renderParties();
+    const PARTIES = await getParties();
+    renderParties(PARTIES);
 }
 render();
+
+//test addParty function
+// let p = {
+//     "name": "BESTparty",
+//     "description": "the worst party",
+//     "date": "2025-03-31T00:00:00.000Z",
+//     "location": "a real house"
+// };
+// addParty(p);
