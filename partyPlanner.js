f667c3ad69c3fc2cf2d04507e579fd4e9d0eb493
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-03-31 12:37:08 -0400

    set up functions

--- partyPlanner.js
+++ partyPlanner.js
@@ -1,1 +1,26 @@
-console.log("does this work");
+const COHORT = "2503-FTB-ET-WEB-FT";
+const API = "https://fsa-crud-2aa9294fe819.herokuapp.com/api";
+// https://fsa-crud-2aa9294fe819.herokuapp.com/api/2109-CPU-RM-WEB-PT/events
+// names, dates, times, locations, and descriptions
+const state = {
+    party: {},
+}
+async function deleteParty(name){
+    
+    render();
+}
+async function getParties(){
+
+}
+async function addParty(){
+
+    render();
+}
+function renderParties(){
+
+}
+async function render(){
+    await getParties();
+    renderParties();
+}
+render();
